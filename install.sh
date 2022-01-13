ba5ac31762ce325f87c8d98c57c23b4ddabf5eac
Mustafa Türk <mtafa.turk@gmail.com>
2022-01-13 21:25:31 +0100

    changes

--- install.sh
+++ install.sh
@@ -15,14 +15,19 @@ brew cask install docker
 brew cask install figma
 brew cask install iterm2
 brew cask install postman
+brew cask install github
+brew cask install hiddenbar
+brew cask install spectacle
 brew cask install visual-studio-code
 
 # install VSCode extensions
-code --install-extension borekb.darker-plus-theme
+code --install-extension formulahendry.auto-rename-tag
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension dsznajder.es7-react-js-snippets
 code --install-extension eamodio.gitlens
 code --install-extension esbenp.prettier-vscode
-code --install-extension formulahendry.auto-rename-tag
-code --install-extension humao.rest-client
 code --install-extension Orta.vscode-jests
+code --install-extension zokugun.explicit-folding
+code --install-extension iocave.customize-ui
+code --install-extension iocave.monkey-patch
+code --install-extension siddharthkp.codesandbox-black
